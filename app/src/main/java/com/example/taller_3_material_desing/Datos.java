5cbc5b430262668fc300d42cfe1b495f2a55c46d
Jose Arevalo <job.38011@gmail.com>
2020-06-11 01:06:52 -0500

    Interfaz Agregar Libro terminada, clase Libro y Datos terminada

--- app/src/main/java/com/example/taller_3_material_desing/Datos.java
+++ app/src/main/java/com/example/taller_3_material_desing/Datos.java
@@ -1,4 +1,14 @@
 package com.example.taller_3_material_desing;
 
+import java.util.ArrayList;
+
 public class Datos {
+    private static ArrayList<Libro> libros = new ArrayList();
+    public static void guardar(Libro p){
+        libros.add(p);
+    }
+
+    public static ArrayList<Libro> obtener(){
+        return libros;
+    }
 }
--- app/src/main/java/com/example/taller_3_material_desing/Libro.java
+++ app/src/main/java/com/example/taller_3_material_desing/Libro.java
@@ -1,4 +1,71 @@
 package com.example.taller_3_material_desing;
 
 public class Libro {
+    private String isbn;
+    private String nombrelibro;
+    private String autorlibro;
+    private String paisautor;
+    private int numeropaginas;
+    private int foto;
+
+    public Libro (String isbn, String nombrelibro, String autorlibro, String paisautor, int numeropaginas, int foto){
+        this.isbn = isbn;
+        this.nombrelibro = nombrelibro;
+        this.autorlibro = autorlibro;
+        this.paisautor = paisautor;
+        this.numeropaginas = numeropaginas;
+        this.foto = foto;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getNombrelibro() {
+        return nombrelibro;
+    }
+
+    public void setNombrelibro(String nombrelibro) {
+        this.nombrelibro = nombrelibro;
+    }
+
+    public String getAutorlibro() {
+        return autorlibro;
+    }
+
+    public void setAutorlibro(String autorlibro) {
+        this.autorlibro = autorlibro;
+    }
+
+    public String getPaisautor() {
+        return paisautor;
+    }
+
+    public void setPaisautor(String paisautor) {
+        this.paisautor = paisautor;
+    }
+
+    public int getNumeropaginas() {
+        return numeropaginas;
+    }
+
+    public void setNumeropaginas(int numeropaginas) {
+        this.numeropaginas = numeropaginas;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public void guardar(){
+        Datos.guardar(this);
+    }
 }
