d624e347fa5397c8f5bb168fd1f094f871537334
Jose Arevalo <job.38011@gmail.com>
2020-06-11 17:08:33 -0500

    Interfaz Detalle del Libro terminada

--- app/src/main/java/com/example/taller_3_material_desing/MainActivity.java
+++ app/src/main/java/com/example/taller_3_material_desing/MainActivity.java
@@ -8,21 +8,40 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        FloatingActionButton fab;
+        RecyclerView lstLibros;
+        ArrayList<Libro> libros;
+        LinearLayoutManager llm;
+        AdaptadorLibro adapter;
 
-        FloatingActionButton fab = findViewById(R.id.btnAgregar);
+        lstLibros = findViewById(R.id.lstLibros);
+        libros = Datos.obtener();
+        llm = new LinearLayoutManager(this);
+        adapter = new AdaptadorLibro(libros);
+
+        llm.setOrientation(RecyclerView.VERTICAL);
+        lstLibros.setLayoutManager(llm);
+        lstLibros.setAdapter(adapter);
+
+        fab = findViewById(R.id.btnAgregar);
     }
 
     public void agregar(View v){
